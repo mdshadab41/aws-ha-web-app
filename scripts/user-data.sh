1e9ace12e6e08a880655ae5caa7d7f5dbc443831
mdshadab41 <shadabjhu00447@gmail.com>
2026-07-10 10:49:02 +0530

    Update user-data script for Ubuntu

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-yum update -y
-yum install -y python3 git
+apt update -y
+apt install -y python3-pip git
 pip3 install flask
 
-git clone https://github.com/<your-username>/aws-ha-web-app.git /home/ec2-user/app
-cd /home/ec2-user/app/app
+git clone https://github.com/mdshadab41/aws-ha-web-app.git /home/ubuntu/app
+cd /home/ubuntu/app/app
 
 nohup python3 app.py > /var/log/flask-app.log 2>&1 &
