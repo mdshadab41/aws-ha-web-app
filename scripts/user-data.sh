aa1bd7c6f4e36a02fcbc46eda9c2ceaa33626ad5
mdshadab41 <shadabjhu00447@gmail.com>
2026-07-10 11:47:47 +0530

    Fix pip install for Ubuntu 26.04 externally-managed-environmen

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update -y
 apt install -y python3-pip git
-pip3 install flask
+pip3 install flask --break-system-packages
 
 git clone https://github.com/mdshadab41/aws-ha-web-app.git /home/ubuntu/app
 cd /home/ubuntu/app/app
